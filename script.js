17690015d9dab833828e8553491f269442f9d999
heXdzh <90160050+heXdzh@users.noreply.github.com>
2023-03-28 14:57:33 +0200

    Added basic  calculating function

---
+++ script.js
@@ -0,0 +1,38 @@
+let numberA = "";
+let numberB = "";
+let operator = "";
+
+function calculate(numberA, operator, numberB)
+{
+    switch (operator)
+    {
+        case "+":
+        {
+        return numberA + numberB;
+        };
+        break;
+
+        case "-":
+        {
+            return numberA - numberB;
+        }
+        break;
+
+        case "*":
+            {
+                return numberA * numberB;
+            }
+        break;   
+            
+        case "/":
+            {
+                return numberA / numberB;
+            }
+        break;
+
+        default:
+        console.log("Wrong input!");    
+    };
+};
+
+console.log(calculate(10, "+", 2));
